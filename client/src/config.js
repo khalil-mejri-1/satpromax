2cd1380889ee69667639c04402f821777df5944a
khalil-mejri-1 <mejrik1888@gmail.com>
2026-05-01 01:44:26 +0100

    final update

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'https://satpromax.com';
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'https://satpromax.com';
+// export const API_BASE_URL = 'http://localhost:3000';
 
 // export const SITE_URL = 'https://satpromax.com'; 
 export const SITE_URL = 'http://localhost:3000';
