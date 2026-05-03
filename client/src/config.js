fba3e2d510c68a532adb77b92fad48102297e977
khalil-mejri-1 <mejrik1888@gmail.com>
2026-05-03 13:47:12 +0100

    final update

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,4 @@
-// export const API_BASE_URL = 'https://satpromax.com';
-export const API_BASE_URL = 'http://localhost:3000';
+const isLocalhost = typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1');
 
-// export const SITE_URL = 'https://satpromax.com'; 
-export const SITE_URL = 'http://localhost:3000';
+export const API_BASE_URL = isLocalhost ? 'http://localhost:3000' : 'https://satpromax.com';
+export const SITE_URL = isLocalhost ? 'http://localhost:5173' : 'https://satpromax.com';
