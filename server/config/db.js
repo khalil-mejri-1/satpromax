90ffa73c412b63b025ee4e506bf01488775d1745
khalil-mejri-1 <mejrik1888@gmail.com>
2026-05-03 15:51:54 +0100

    final update

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         const options = {
-            maxPoolSize: 10, // Maintain up to 10 socket connections
-            serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-            family: 4 // Use IPv4, skip trying IPv6
+            maxPoolSize: 10,
+            serverSelectionTimeoutMS: 5000
         };
         await mongoose.connect("mongodb+srv://technoplus989_db_user:r2G0uyv5WI19dZU6@cluster0.oxhvidw.mongodb.net/technoplus?appName=Cluster0", options);
         console.log("MongoDB connected ✅");
