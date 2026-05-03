add52175eb2cc67f462d1557dddd4ff97ca5acc2
khalil-mejri-1 <mejrik1888@gmail.com>
2026-05-03 16:15:02 +0100

    final update

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const options = {
-            maxPoolSize: 10,
-            serverSelectionTimeoutMS: 5000
-        };
-        await mongoose.connect("mongodb+srv://technoplus989_db_user:r2G0uyv5WI19dZU6@cluster0.oxhvidw.mongodb.net/technoplus?appName=Cluster0", options);
+        await mongoose.connect("mongodb+srv://technoplus989_db_user:r2G0uyv5WI19dZU6@cluster0.oxhvidw.mongodb.net/technoplus?appName=Cluster0");
         console.log("MongoDB connected ✅");
     } catch (error) {
         console.error("DB connection error ❌", error);
